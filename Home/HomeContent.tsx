340ba7b5f1d8685841637c8dd0c8dbb6ba75fde3
Rhaian Alvarado <alvaradorhaian766@gmail.com>
2026-02-23 05:43:30 -0300

    Atualizando o modelo

--- Home/HomeContent.tsx
+++ Home/HomeContent.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import Banner from "@/components/site/Banner/Principal";
+import CategoriasDestaque from "@/components/site/categoria/CategoriasDestaque";
+
 import Navbar from "@/components/site/menu/navbar";
 
 
