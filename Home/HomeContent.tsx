a7764cf51d1089c9e4eac2d4e41e82d2f1063065
Rhaian Alvarado <alvaradorhaian766@gmail.com>
2026-02-23 06:27:22 -0300

    Alteraçao

--- Home/HomeContent.tsx
+++ Home/HomeContent.tsx
@@ -2,18 +2,24 @@
 
 import Banner from "@/components/site/Banner/Principal";
 import Categoria from "@/components/site/categoria/Categoria";
+import Cupons from "@/components/site/Faixa/FaixaChamada";
 
 
 import Navbar from "@/components/site/menu/navbar";
+import ProdutoDestaque from "@/components/site/produto/ProdutoDestaque";
+import FooterPrincipal from "@/components/site/Rodape/Footer";
 
 
 
 export default function HomeContent() {
   return (
     <>
-    <Navbar />
-    <Banner />
-    <Categoria />
+      <Navbar />
+      <Banner />
+      <Categoria />
+      <ProdutoDestaque />
+      
+      <FooterPrincipal />
     </>
   );
 }
