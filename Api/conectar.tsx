7bc652aafa2749f0a4cd6bc3536d766cbd9db283
Rhaian Alvarado <alvaradorhaian766@gmail.com>
2026-04-01 02:07:10 -0300

    mmdc

--- Api/conectar.tsx
+++ Api/conectar.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: 'https://lightgrey-cattle-160990.hostingersite.com',
   headers: {
     "Content-Type": "application/json",
   },
