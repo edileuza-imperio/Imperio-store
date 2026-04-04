fa8fa707cc8c45bcfbf543dcc74a227f67cbb97e
Rhaian Alvarado <alvaradorhaian766@gmail.com>
2026-04-03 21:42:01 -0300

    add

--- app/Admin/Banners/page.tsx
+++ app/Admin/Banners/page.tsx
@@ -1,8 +1,7 @@
 export default function BannersPage() {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Banners</h1>
-      <p>Manage your banners here.</p>
+      
     </div>
   );
 }
