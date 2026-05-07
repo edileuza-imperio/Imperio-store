0fa97a34328ab1be2f799678443f7fb4fef3982a
Rhaian Alvarado <alvaradorhaian766@gmail.com>
2026-05-07 18:44:30 -0300

    arrumado

--- app/Carrinho/checkout/page.tsx
+++ app/Carrinho/checkout/page.tsx
@@ -1,7 +1,7 @@
 export default function CheckoutPage() {
   return (
     <div>
-      <h1>Checkout</h1> 
+      <h1>Checkoutdsdsd</h1> 
       <p>Em breve, a página de checkout estará disponível.</p>
     </div>
   );
