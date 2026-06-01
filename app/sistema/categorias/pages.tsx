24cd52df7d9004644919ff46c0feeb180b6b9494
Rhaian Alvarado <alvaradorhaian766@gmail.com>
2026-05-31 22:04:51 -0300

    add

---
+++ app/sistema/categorias/pages.tsx
@@ -0,0 +1,12 @@
+import DashboardLayout from "@/components/pages/painel/layout";
+
+export default function Page() {
+  return (
+   
+      <div>
+        <h1>Categoria</h1>
+        <p>Gerencie suas categorias aqui.</p>
+      </div>
+    
+  );
+}
