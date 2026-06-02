7b822e35adf271414b7e4d880d71dacde9894c8b
Rhaian Alvarado <alvaradorhaian766@gmail.com>
2026-06-02 15:40:50 -0300

    add

--- components/site/Banner/Banner.tsx
+++ components/site/Banner/Banner.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import { imagemFundo } from "@/components/Bibioteca/imagem";
-import styles from "./banner.module.css";
+import styles from "./Banner.module.css";
 import { useBanner } from "./useBanner";
 
 export default function Banner() {
