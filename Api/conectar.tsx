5c4efbeaa19b94d42441ec5ee96a3895138d2e98
Rhaian Alvarado <alvaradorhaian766@gmail.com>
2026-06-06 04:19:51 -0300

    add

--- Api/conectar.tsx
+++ Api/conectar.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://api.universoimperio.com.br",
   headers: {
     "Content-Type": "application/json",
   },
