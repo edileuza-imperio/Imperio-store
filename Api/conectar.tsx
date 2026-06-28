1718a188540f0972a748ee00de9901b4b92b542e
Rhaian Alvarado <alvaradorhaian766@gmail.com>
2026-06-27 22:19:16 -0300

    g

--- Api/conectar.tsx
+++ Api/conectar.tsx
@@ -1,17 +1,24 @@
 import axios, { AxiosInstance } from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-if (!API_URL) {
-  throw new Error("NEXT_PUBLIC_API_URL sdsd não foi definida.");
-}
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://api.universoimperio.com.br/api/v1";
 
 const api: AxiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
-    "Content-Type": "application/json",
+    Accept: "application/json",
   },
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("Erro na requisição da API:", error);
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
